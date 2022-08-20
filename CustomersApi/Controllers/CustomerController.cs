eb72baf25e20fcc419f7bbf38f3d70777f009556
Agussangari <agustin.sangari@gmail.com>
2022-08-20 13:23:42 -0300

    secondCommit

---
+++ CustomersApi/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomersApi.dtos;
+
+namespace CustomersApi.Controllers
+{
+    [ApiController]
+
+    [Route("api/[controller]")]
+    
+    public class CustomerController : Controller
+    {
+        //api//customer
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+
+        //public async Task<List<CustomerDto>> GetCustomers()
+        public async Task<ActionResult> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        //api//customer/{id}
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(CustomerDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        ///public async Task<CustomerDto> GetCustomer(long id)
+        public async Task<IActionResult> GetCustomer(long id)
+        {
+            var vacio = new CustomerDto();
+
+            return new OkObjectResult(vacio);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CustomerDto))]
+
+        public async Task<IActionResult> CreateCustomer(CreateCustomerDto customer)
+        {
+            var vacio = new CustomerDto();
+
+            return new CreatedResult($"http://localhost:7263/api/customer/{vacio.Id}", null);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+        public async Task<ActionResult> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ CustomersApi/Repositories/CustomerDatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using CustomersApi.dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CustomersApi.Repositories
+{
+    public class CustomerDatabaseContext : DbContext
+    {
+        //aca pongo el mismo nombre de la tabla desde aca ya se puede acceder a la tabla con este caso de uso.
+        public DbSet<CustomerEntity> Customer { get; set; }
+
+        public async Task<CustomerEntity> Get(long id)
+        {
+            return await  Customer.FirstAsync(x => x.Id == id);
+        }
+
+        
+        public async Task<CustomerEntity> Add(CreateCustomerDto customerDto)
+        {
+            CustomerEntity entity = new CustomerEntity()
+            {
+                Id = null,
+                Address = customerDto.Address,
+                Email = customerDto.Email,
+                FirstName = customerDto.FirstName,
+                LastName = customerDto.LastName,    
+            };
+            EntityEntry<CustomerEntity> response = await Customer.AddAsync(entity);
+            await SaveChangesAsync();
+            return await Get(response.Entity.Id ?? throw new Exception("No se ha podido guardar"));
+        }
+    }
+
+    public class CustomerEntity
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
